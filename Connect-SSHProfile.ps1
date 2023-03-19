a8c284fabd57feb7ad913e7d2e9a2246118b0d4d
schmankerl <43405925+schmankerl@users.noreply.github.com>
2023-03-19 21:40:26 +0100

    changed formatting of functions

--- Connect-SSHProfile.ps1
+++ Connect-SSHProfile.ps1
@@ -1,24 +1,24 @@
 # Json File Path
 $sshProfiles = Get-Content -Path "" -Raw | ConvertFrom-Json
 
-function Connect-SSHProfile($profileName) {
+function Connect-SSHProfile($profileName) 
+{
     $sshProfile = $sshProfiles | Where-Object { $_.Name -eq $profileName }
-    if ($sshProfile) {
+    if ($sshProfile) 
+    {
         $sshCommand = "ssh.exe $($sshProfile.Username)@$($sshProfile.Hostname)"
         if ($sshProfile.Port) { $sshCommand += " -p $($sshProfile.Port)" }
 
         Write-Host "Connecting to $($sshProfile.Name)..."
         Invoke-Expression $sshCommand
-    } else {
-        Write-Error "Profile does not exist: $profileName"
-    }
+    } 
+    else { Write-Error "Profile does not exist: $profileName" }
 }
 
-function Get-SSHProfiles {
+function Get-SSHProfiles 
+{
     Write-Host "SSH Profiles:"
-    $sshProfiles | ForEach-Object {
-        Write-Host " - $($_.Name) ($($_.Username)@$($_.Hostname):$($_.Port))"
-    }
+    $sshProfiles | ForEach-Object { Write-Host " - $($_.Name) ($($_.Username)@$($_.Hostname):$($_.Port))" }
 }
 
 #Usage
--- Edit-Profile.ps1
+++ Edit-Profile.ps1
@@ -1,12 +1,12 @@
 #Function which either opens the current/specified profile or creates a new one if it does not exist
-function Edit-Profile {
-    param (
-        [string]$Name = "Current User"
-    )
+function Edit-Profile 
+{
+    param ([string]$Name = "Current User")
     
     $profilePath = $profile -replace "\\.ps1$",".ps1"
     
-    if (!(Test-Path $profilePath)) {
+    if (!(Test-Path $profilePath)) 
+    {
         New-Item -ItemType File -Path $profilePath -Force | Out-Null
     }
     
