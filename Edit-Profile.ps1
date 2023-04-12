96ff1c5582cdf820c72a835054f30dd57ffacb71
schmankerl <43405925+schmankerl@users.noreply.github.com>
2023-04-12 22:17:46 +0200

    Modified Edit-Profile to allow to specify the wanted 'Word processor'

--- Edit-Profile.ps1
+++ Edit-Profile.ps1
@@ -1,4 +1,28 @@
 #Function which either opens the current/specified profile or creates a new one if it does not exist
+#Needs to have $env:EDITOR variable set in order for this to work
+function Edit-Profile 
+{
+    param ([string]$Name = "Current User")
+    
+    $profilePath = $profile -replace "\\.ps1$",".ps1"
+    
+    if (!(Test-Path $profilePath)) 
+    {
+        New-Item -ItemType File -Path $profilePath -Force | Out-Null
+    }
+    
+    $editorCommand = $env:EDITOR
+    if ([string]::IsNullOrWhiteSpace($editorCommand)) 
+    {
+        $editorCommand = "notepad.exe"
+    }
+    
+    Start-Process $editorCommand -ArgumentList $profilePath
+}
+
+#Usage
+#Edit-Profile -Name [POWERSHELL_PROFILE_NAME]
+
 function Edit-Profile 
 {
     param ([string]$Name = "Current User")
